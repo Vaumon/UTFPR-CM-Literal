f310a7efbf5e7daf83873b381a0c76ec6ae04d01
vaumon <carlossanchesdorneles@gmail.com>
2013-08-21 22:53:21 -0300

    Criando Testes

---
+++ src/LiteralJuris/target/classes/Teste/Test.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Teste;
+
+/**
+ *
+ * @author a1265067
+ */
+public class Test {
+    
+    
+    Test(){
+        
+    }
+    
+    public boolean validaLogin(String login,String senha){
+        return false;
+    }
+}
